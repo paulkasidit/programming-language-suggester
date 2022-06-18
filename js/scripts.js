b9349f4a8ecf41d5a3bf507e3d69750d38ef6e2f
Paul Kasidit <paulkasidit@gmail.com>
2022-06-18 15:47:50 -0700

    completing quiz logic, will be testing it soon

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,20 @@ console.log("Script running");
 //Business Logic 
   //first hide the results area  
 
-const pythonProgrammingLanguage = "Python is known for simple syntax. It's the number one preferred language for data analysts.";
-const rubyProgrammingLanguage = "Ruby On Rails is your programming language. Known for its unique syntax and versatality."; 
-const swiftProgrammingLanguage = "Swift is best for developing mobile apps. Famous examples include apps for iOS.";  
-
-function calculateDataAnalyst(num){ //function called if user selects 
-  if (num === 6){
-    return rubyProgrammingLanguage;
-  };
-};
-
-function calculateWebDeveloper(num){
-  if (num === 6){
-    return rubyProgrammingLanguage;
-  };
-};
-
-function calculateAppDeveloper(num){
-  
-
-};
+const pythonProgrammingLanguage = "Python is known for simple syntax. It's the number one preferred language for data analysts."; //5
+const rubyProgrammingLanguage = "Ruby On Rails is your programming language. Known for its unique syntax and versatality.";  //6
+const swiftProgrammingLanguage = "Swift is best for developing mobile apps. Famous examples include apps for iOS.";  //7 
 
 function generateProgrammingLanguage(event){
   event.preventDefault();
   console.log("generateProgrammingLanguage running")
   //getting elements from the form
   const fieldOfWork = document.querySelector("input[name='fieldOfWork']:checked").value;  // data analyst  = 5, web developer = 6, app developer = 7 
-  const creativeAnalytical = document.querySelector("input[name='creativeAnalytical']:checked").value; //creative val  = 3, analytical val  = 4  
-  const neatMessy = document.querySelector("input[name='neatMessy']:checked").value; //neat val = 3 , messy vall = 4
-  const gemsSnakes = document.querySelector("input[name='gemsSnakes']:checked").value; // gems val  = 3, snakes val = 4 
+  const creativeAnalytical = document.querySelector("input[name='creativeAnalytical']:checked").value; //creative val  = 0, analytical val  = 1  
+  const neatMessy = document.querySelector("input[name='neatMessy']:checked").value; //neat val = 0 , messy val = 1
+  const gemsSnakes = document.querySelector("input[name='gemsSnakes']:checked").value; // gems val  = 0, snakes val = 1 
   const stressScale = parseInt(document.querySelector("input[name='stressScale']:checked").value); //range from 8 - 12 based on respective value 
-  
+   
   //See if element is returned to page 
   console.log(fieldOfWork);
   console.log(creativeAnalytical);
@@ -41,21 +24,48 @@ function generateProgrammingLanguage(event){
   console.log(gemsSnakes);
   console.log(stressScale);
 
-  let result = 0; //initial value is set to 0 
+  let careerField; //intialize intial variable and assign a numerical value to the variable; 
   //determine the career path user wants to take  
-  
   if (fieldOfWork === "dataAnalytics"){
-    score = 5; 
+    careerField = 5; 
+  }
+  else if (fieldOfWork === "webDevelopment"){
+    careerField = 6; 
+  };
+  else if (fieldOfWork === "appDevelopment"){
+    careerField = 7; 
+  };
+
+  let messy = 0;
+  let analytical = 0; 
+  let snakes = 0;  
+  //Starting with the most simple case, Python is the most logical choice for data analyst; 
+  if (neatMessy === 'messy'){ 
+    messy += 1; 
+  }; else if (gemsSnakes === "snakes"){ 
+    snakes += 1; 
+  } else if  (creativeAnalytical = 'analytical'){
+    analytical += 1
   }
 
-  if (fieldOfWork === "webDevelopment"){
-    score = 6; 
-    calculateWebDeveloper(score);
-  };
+  let result;
+  if (fieldOfWork === 5 ){
+    result = pythonProgrammingLanguage;
+  }; 
 
-  if (fieldOfWork === "appDevelopment"){
-    score = 7; 
-  };
+  if (fieldOfWork === 6 && stressScale >= 3 && snakes = 1){
+    result = pythonProgrammingLanguage + "Python also offers web development frameworks like Django. Though, the learning curve may be steep. Ruby on Rails may be a safer option."; 
+  } else {
+    result = rubyProgrammingLanguage; 
+  }; 
+
+  if (fieldOfWork === 7 && analytical === 1 && snakes = 0){
+    result = rubyProgrammingLanguage + "Ruby offers support for app development backends.";
+  } else { 
+    result = swiftProgrammingLanguage;
+  }
+
+  
 
 };
 
