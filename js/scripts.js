88915cb5a8ef07179902bb7f84cc02c184aa9830
Paul Kasidit <paulkasidit@gmail.com>
2022-06-18 14:38:17 -0700

    managing to get the selecetd form elements to be sent to the backend

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
 // choice possibilties: Python, Ruby On Rails, Swift
-window.onload = function(){ 
 console.log("Script running");
 //Business Logic 
   //first hide the results area  
-  let resultsArea = document.getElementById("results-area");
-  resultsArea.style.display = "none";
 
-  const form = document.querySelector(progammingLanguageSuggestor)
 
-  function generateProgrammingLanguage(event){
-    console.log("generateProgrammingLanguage running")
+function generateProgrammingLanguage(event){
+  event.preventDefault();
+  console.log("generateProgrammingLanguage running")
   //getting elements from the form
-    const fieldOfWork =  toString(document.querySelector("input[name="fieldOfWork"]:checked").value);
-    const creativeAnalytical = toString(document.querySelector("input[name="creativeAnalytical"]:checked").value);
-    const neatMessy = toString(document.querySelector("input[name="neatMessy"]:checked").value);
-    const gemsSnakes = toString(document.querySelector("input[name="gemsSnakes"]:checked").value);
-    const stressScale = parseInt(document.querySelector("input[name="stressScale"]:checked").value);
+  const fieldOfWork = document.querySelector("input[name='fieldOfWork']:checked").value;
+  const creativeAnalytical = document.querySelector("input[name='creativeAnalytical']:checked").value;
+  const neatMessy = document.querySelector("input[name='neatMessy']:checked").value;
+  const gemsSnakes = document.querySelector("input[name='gemsSnakes']:checked").value;
+  const stressScale = parseInt(document.querySelector("input[name='stressScale']:checked").value);
   
-    //See if element isreturned to page 
-    console.log(fieldOfWork);
-    console.log(creativeAnalytical);
-    console.log(neatMessy);
-    console.log(gemsSnakes);
-    console.log(stressScale);
-  };
+  //See if element is returned to page 
+  console.log(fieldOfWork);
+  console.log(creativeAnalytical);
+  console.log(neatMessy);
+  console.log(gemsSnakes);
+  console.log(stressScale);
 
-  form.addEventListener("submit", generateProgrammingLanguage)
 
-}; 
+};
+
+window.addEventListener("load", function(){
+  const form  = document.getElementById("progammingLanguageSuggestor");
+  form.addEventListener("submit", generateProgrammingLanguage);
+});
+
